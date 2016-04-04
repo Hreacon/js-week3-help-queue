0e1e20a4af35f65a33dd1d5ed68ab54dd668c99f
jordan meier and Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-04-04 09:25:57 -0700

    create a ticket view component to allow teachers to view individual tickets

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('queue');
   this.route('ticket', { path: 'ticket/:ticket_id'});
+  this.route('ticket-view', {path: 'ticket-view/:ticket_id'});
 });
 
 export default Router;
